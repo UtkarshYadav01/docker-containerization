66c807674011d708e0bd6dc385bfcab06d30ce1f
UtkarshYadav01 <67846943+UtkarshYadav01@users.noreply.github.com>
2026-04-06 07:45:38 +0530

    init: spring boot project setup with maven

---
+++ src/main/java/com/y/utk/docker/DockerContainerizationApplication.java
@@ -0,0 +1,13 @@
+package com.y.utk.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerContainerizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerContainerizationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/y/utk/docker/DockerContainerizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.y.utk.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerContainerizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
